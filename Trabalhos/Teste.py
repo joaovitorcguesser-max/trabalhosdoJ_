7a4e7896d60a12bdf07c6b3086759ed90d821643
joaovitorcguesser-max <joaovitorcguesser@gmail.com>
2026-02-06 17:03:21 -0300

    Add files via upload

---
+++ Trabalhos/Teste.py
@@ -0,0 +1,7 @@
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digit a segunda nota: '))
+media = (nota1 + nota2) / 2
+if media >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
